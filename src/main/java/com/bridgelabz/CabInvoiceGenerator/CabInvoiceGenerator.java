47f09104e44c85d3e85d378b8bade8090e103f63
Rajat Gundi <glrajat@gmail.com>
2020-11-09 12:34:36 +0530

    Added: Feature To Show AverageFare And Display NumberOfRides

--- src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -4,22 +4,17 @@ import java.util.ArrayList;
 
 public class CabInvoiceGenerator
 {
-	static ArrayList<CabRide> list = new ArrayList<CabRide>();
-	public static double sum()
-	{
-		double sum=0;
-		for(CabRide i:list)
-		{
-			sum+=i.fairCalculator();
-		}
-		return sum;
-	}
 	public static void main(String args[])
 	{	
+		CabRideRecorder record = new CabRideRecorder();
 		CabRide ride = new CabRide(2f,10f);
 		CabRide ride2 = new CabRide(5f,2f);
-		list.add(ride);
-		list.add(ride2);
-		System.out.println("Rs"+sum());
+		CabRide ride3 = new CabRide(5f,2f);
+		record.add(ride);
+		record.add(ride2);
+		record.add(ride3);
+		System.out.println("Aggregate Ride Fare: Rs"+record.sum());
+		System.out.println("Number Of Rides: "+record.numberOfRides());
+		System.out.println(String.format("Average Fare: Rs %1$,.2f",record.average()));
 	}
 }
